387e6a98051ef4a80948699b9e19778c53b05602
NXFORGE <158617734+nxforge@users.noreply.github.com>
2026-03-12 00:58:44 +0300

    Remove unnecessary lines from client.py

--- client.py
+++ client.py
@@ -4,6 +4,3 @@ import requests
 
 def post(ip: str, message: str):
     requests.post(f"http://{ip}", json={"message": message})
-
-if __name__ == "__main__":
-    post("192.168.0.106:63323", "Мыло")
